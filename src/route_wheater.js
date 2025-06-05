d4e7afe8f11d0fbdcc9dc7a4db6d0e722aeb3578
gabriel <gabrieltertoprezoto@gmail.com>
2025-06-05 16:35:47 -0300

    html adding

--- src/route_wheater.js
+++ src/route_wheater.js
@@ -15,13 +15,57 @@ router.get('/washington', async (req, res) =>{
 
     try {
         const cached = await redisClient.get(cacheK)
+
+        const parsed = JSON.parse(cached)
+        const climaPars = parsed.currentConditions
+
+        const fahrenheit = climaPars.temp
+        const celsius = ((fahrenheit - 32) * 5 / 9).toFixed(1)
+
+
         if (cached) {
-            return res.json({ source: 'cache', data: JSON.parse(cached) })
+            // return res.json({ source: 'cache', data: JSON.parse(cached) })
+
+
+            return res.send(`
+            <html>
+                <head>
+                    <title>Clima de Washington</title>
+                </head>
+                <body>
+                    <h1>Você buscou o clima de Washington</h1>
+                    <h2>temperatura: ${celsius}°C</h2>
+                </body>
+            </html>    
+            `)
         }
 
+
+        // SERVE PARA FAZER O CÓDIGO SER DINÂMICO
+        // PERMITE BUSCAR O CLIMA DA CIDADE QUE QUISER(SÓ QUE ATUALMENTE 
+        // SÓ ESTÁ BUSCANDO O CLIMA DE washington)
         const weater = await searcWheater(city) // a chamada da função pode não ter nenhum parametro
         await redisClient.setEx(cacheK, 3600, JSON.stringify(weater))
-        res.json({ source: 'api', data: weater })
+        
+
+        const clima = weater.currentConditions
+
+
+        //RETORNA O JSON COM AS INFORMAÇÕES DE CLIMA
+        // res.json({ source: 'api', data: weater })
+
+         res.send(`
+            <html>
+                <head>
+                    <title>Clima de Washington</title>
+                </head>
+                <body>
+                    <h1>Você buscou o clima de Washington</h1>
+                    <h2>temperatura: ${clima.temp}</h2>
+                </body>
+            </html>    
+            `)
+
     } catch (err) {
         console.log(err)
         res.status(500).json({ error: 'erro ao buscar dados refentes ao clima' })
